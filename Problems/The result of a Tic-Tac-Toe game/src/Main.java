b41becf4518e9e1cd23ff2f21d39b886a90aa47e
Javier Balsas <javier9518@gmail.com>
2020-05-29 00:50:28 -0500

    Problemas hasta la seccion 4

---
+++ Problems/The result of a Tic-Tac-Toe game/src/Main.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+//        String[] myNum = {"O","X","X","O","X","O","X","O","X"};
+//        int count = 0;
+//        for(int i=0; i < myNum.length; i++){
+//            count++;
+//            System.out.print(myNum[i]+" ");
+//            //System.out.println(i);
+//            if(count==3){
+//                System.out.println("");
+//                count = 0;
+//            }
+//
+//
+//        }
+
+        System.out.print("O X X");
+        System.out.println("");
+        System.out.print("O X O");
+        System.out.println("");
+        System.out.print("X O X");
+        System.out.println("");
+    }
+}
